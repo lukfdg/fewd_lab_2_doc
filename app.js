9d791809a391d4aea402acadc9286881c80b4acb
Lucas_Souza <lukfdg@gmail.com>
2020-02-04 14:46:43 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,60 @@
+var myAlphabet;
+myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+console.log(myAlphabet.length);
+function myAlphabetLength() {
+console.log(myAlphabet.length);
+}
+myAlphabetLength();
+
+var planets;
+planets=["Mercurio", "Venus", "Terra", "Marte", "Jupiter"];
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+var wowDatatypes;
+wowDatatypes = [20, "Lucas", 04/02];
+console.log(wowDatatypes);
+
+var x=5, y=10, z=15;
+console.log(x+y);
+console.log(y-x);
+console.log(x*y);
+console.log(y/x);
+console.log(x%2);
+console.log(x++);
+console.log(x--);
+
+var a=5, b=' hello ', c=' world ', d=10;
+console.log(a+b);
+console.log(b+c);
+console.log(a+d);
+
+var a=5, b=10, c='5';
+var x=a;
+console.log(a==c);
+console.log(a===c);
+console.log(a==x);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a>=c);
+console.log(a<=c);
+
+var hello=function(){
+    return 'Hello World!';
+}
+var result=hello();
+console.log(result);
+
+var hello2=function(){
+    return 'hi!';
+};
+var x=hello2();
+console.log(x);
+
+
